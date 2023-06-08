4edc299dc0169aa489e6cbf9751eba0432a235bf
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-06-08 23:47:00 +0300

    completed task 2

--- 2_cli_primitive_db/index.js
+++ 2_cli_primitive_db/index.js
@@ -2,35 +2,35 @@ const fs = require('fs').promises;
 const path = require('path');
 const inquirer = require('inquirer');
 
-const databaseFile = path.resolve('./users.txt')
+const databaseFile = path.resolve('./users.txt');
 
 function addUser() {
   inquirer
     .prompt([
-    {
+      {
         type: 'input',
         name: 'name',
-        message: 'Enter the user\'s name. To cansel press ENTER:',
-        },
+        message: "Enter the user's name. To cansel press ENTER:",
+      },
     ])
-     .then((answers) => {
-      const { name } = answers;
-      if (name.trim() !== '') {
+      .then(answers => {
+        const { name } = answers;
+        if (name.trim() !== '') {
         inquirer
           .prompt([
             {
               type: 'list',
               name: 'gender',
-              message: 'Choose the gender:',
-              choices: ['Male', 'Female'],
+              message: 'Choose your gender:',
+              choices: ['male', 'female'],
             },
             {
               type: 'number',
               name: 'age',
-              message: 'Enter the age:',
+              message: 'Enter your age:',
             },
           ])
-          .then((answers) => {
+            .then(answers => {
             const { gender, age } = answers;
             const user = {
               name,
@@ -39,16 +39,16 @@ function addUser() {
             };
             const userData = JSON.stringify(user);
             fs.appendFile(databaseFile, userData, 'utf8');
-            addUser(); // Repeat the cycle to add another user
+            addUser();
           });
       } else {
-        findUser(); // Prompt to find a user after adding users
+        selectedUser();
       }
     });
 }
 
-function findUser() {
-    inquirer
+function selectedUser() {
+  inquirer
     .prompt([
       {
         type: 'confirm',
@@ -56,39 +56,40 @@ function findUser() {
         message: 'Would you like to search values in DB?:',
       },
     ])
-      .then(async (answers) => {
-        const { search } = answers;
-        if (search) {
-            const list = await fs.readFile(databaseFile, "utf-8");
-            const users = list.split('\n');
-            let foundUser = false;
-            let userData = [];
-            for (const user of users) {
-                if (user.trim() !== '') {
-                userData.push(JSON.parse(user)); 
-                }
-            }
-            console.log(userData);
-             //   if (userData.name.toLowerCase() === search.toLowerCase()) {
-            //     console.log('User found:');
-            //     console.log('Name:', userData.name);
-            //     console.log('Gender:', userData.gender);
-            //     console.log('Age:', userData.age);
-            //     console.log();
-            //     foundUser = true;
-            //     break;
-            //   }
-
-
-          if (!foundUser) {
-            console.log('User not found.\n');
+    .then(async answers => {
+      const { search } = answers;
+      if (search) {
+        const list = await fs.readFile(databaseFile, 'utf-8');
+        const users = list.split('\n');
+        let userData = [];
+        for (const user of users) {
+          if (user.trim() !== '') {
+            userData.push(JSON.parse(user));
           }
         }
-        else {
-        console.log('Exiting the application.');
+        console.log(userData);
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              name: 'name',
+              message: "Enter the user's name you wanna find in DB:",
+            },
+          ])
+          .then(answers => {
+            const { name } = answers;
+            const selectedUser = userData.find(
+              user => user.name.toLowerCase() === name.toLowerCase()
+            );
+            if (name.trim() !== '' && selectedUser) {
+              console.log(`User ${name} was found`);
+              console.log(selectedUser);
+            } else {
+              console.log('Such user does not exist.');
+            }
+          });
       }
-      });
+    });
 }
 
-// Start the application
-addUser(); // Prompt to add a user
+addUser(); 
