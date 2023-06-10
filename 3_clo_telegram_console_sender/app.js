5fa47483b9d1a09fada9797bedf774f95c613c30
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-06-10 16:26:28 +0300

    add short command

--- 3_clo_telegram_console_sender/app.js
+++ 3_clo_telegram_console_sender/app.js
@@ -5,22 +5,25 @@ const { program } = require('commander');
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = '6032565948:AAGouNkYRNmnK1IiYPRv6DrM6ao13TBGf3I';
-const bot = new TelegramBot(TOKEN, {polling: true});
 const CHAT_ID = "534196505"
+
+const bot = new TelegramBot(TOKEN, {polling: true});
 
 program
   .command('message <message>')
   .description('Send a message to the Telegram bot')
+  .alias('m')
   .action(async(message) => {
     await bot.sendMessage(CHAT_ID, message)
   process.exit(0);
   });
 
 program
-  .command('photo <photoPath>')
-  .description('Send a photo to the Telegram bot')
-  .action(async(photoPath) => {
-    await bot.sendPhoto(CHAT_ID, photoPath);
+  .command('photo <path>')
+  .description('Send a photo to the Telegram bot. Just drag and drop it to consol after p-flag.')
+  .alias('p')
+  .action(async(path) => {
+    await bot.sendPhoto(CHAT_ID, path);
   process.exit(0);
   });
 
