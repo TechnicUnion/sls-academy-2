744248bfd9b6eecccf3dcc62dbe557d854b95972
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-06-20 01:00:12 +0300

    completed task 6

--- 6_instagram_giveaway/app.js
+++ 6_instagram_giveaway/app.js
@@ -5,9 +5,8 @@ function readFile(file) {
   return fs.readFileSync(file, 'utf-8').split('\n');
 }
 
-function uniqueValues() {
-  const startTime = Date.now();
 
+function uniqueValues() {
   const uniqueUsernames = new Set();
   for (let i = 1; i < 20; i++) {
     const filePath = path.join(__dirname, `./text/out${i}.txt`);
@@ -15,18 +14,10 @@ function uniqueValues() {
     words.forEach(word => uniqueUsernames.add(word));
   }
 
-  const count = uniqueUsernames.size;
-
-  const elapsedTime = (Date.now() - startTime) / 1000;
-  console.log(`Elapsed time for uniqueValues(): ${elapsedTime.toFixed(2)} seconds`);
-
-  return count;
+  return uniqueUsernames.size;
 }
 
-
 function existInAllFiles() {
-  const startTime = Date.now();
-
   let commonUsernames = null;
   for (let i = 1; i < 20; i++) {
     const filePath = path.join(__dirname, `./text/out${i}.txt`);
@@ -38,70 +29,33 @@ function existInAllFiles() {
     }
   }
 
-  const count = commonUsernames ? commonUsernames.size : 0;
-
-  const elapsedTime = (Date.now() - startTime) / 1000;
-  console.log(`Elapsed time for existInAllFiles(): ${elapsedTime.toFixed(2)} seconds`);
-
-  return count;
+  return commonUsernames ? commonUsernames.size : 0;
 }
 
-// const fileOccurrence = Array(20).fill(0);
-// function existInAtleastTen() {
-//   const startTime = Date.now();
-
-
-//   for (let i = 1; i < 20; i++) {
-//     const filePath = path.join(__dirname, `./text/out${i}.txt`);
-//     const words = readFile(filePath);
-//     const uniqueWords = new Set(words);
-//     uniqueWords.forEach(word => fileOccurrence[i - 1]++);
-//   }
-
-//   const count = fileOccurrence.filter(occurrence => occurrence >= 10).length;
-
-//   const elapsedTime = (Date.now() - startTime) / 1000;
-//   console.log(`Elapsed time for existInAtleastTen(): ${elapsedTime.toFixed(2)} seconds`);
-
-//   return count;
-// }
-
 function existInAtleastTen() {
-  const startTime = Date.now();
-
-  const usernameCountMap = new Map();
+  const usernameCount = {};
   for (let i = 1; i < 20; i++) {
     const filePath = path.join(__dirname, `./text/out${i}.txt`);
     const words = readFile(filePath);
     const uniqueWords = new Set(words);
     uniqueWords.forEach(word => {
-      const count = usernameCountMap.get(word) || 0;
-      usernameCountMap.set(word, count + 1);
+      usernameCount[word] = (usernameCount[word] || 0) + 1;
     });
   }
 
-  const count = Array.from(usernameCountMap.values()).filter(occurrence => occurrence >= 10).length;
-
-  const elapsedTime = (Date.now() - startTime) / 1000;
-  console.log(`Elapsed time for existInAtleastTen(): ${elapsedTime.toFixed(2)} seconds`);
-
+  const count = Object.values(usernameCount).filter(occurrence => occurrence >= 10).length;
   return count;
 }
 
-
-
-
-
-
-
-
-// Example usage
+const startTime = Date.now();
 const uniqueCount = uniqueValues();
 const allFilesCount = existInAllFiles();
 const atLeastTenCount = existInAtleastTen();
+const elapsedTime = (Date.now() - startTime) / 1000;
+console.log(`Elapsed time: ${elapsedTime.toFixed(2)} seconds`);
+
 
 console.log("Unique usernames in all files:", uniqueCount);
 console.log("Usernames occurring in all 20 files:", allFilesCount);
 console.log("Usernames occurring in at least 10 files:", atLeastTenCount);
 
-// uniqueValues();
